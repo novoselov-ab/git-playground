2e754b76fa2c68d4ab148921600e543af120769a
Jason Maskell <backov@tamedtornado.com>
2015-11-15 13:53:28 +0100

    Removed Flex WIP

--- Engine/Plugins/GameWorks/Flex/Source/Flex/Private/FlexRopeActor.cpp
+++
@@ -1,8 +0,0 @@
-#include "FlexPCH.h"
-
-AFlexRopeActor::AFlexRopeActor(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-	FlexRopeComponent = ObjectInitializer.CreateDefaultSubobject<UFlexRopeComponent>(this, TEXT("FlexRopeComponent0"));
-	RootComponent = FlexRopeComponent;
-}
